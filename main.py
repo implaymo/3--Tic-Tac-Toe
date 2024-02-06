2b292c0e2cb64cef77daa47ec721ccfc897e5606
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-06 16:09:45 +0000

    Change board list. Made each part of the table a char.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-board = ["_|_|_"]
+board = ["_", "|", "_", "|", "_"]
 
 loop_counter = 1
 
@@ -6,4 +6,5 @@ while loop_counter < 3:
     loop_counter += 1
     print(*board)
     if loop_counter == 3:
-        print(" | | ")
+        print(" ", "|", " ", "|"," ")
+
