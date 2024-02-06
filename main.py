2d0fc31a08d3304e27a5945b8e7a2d255ee40971
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-06 16:45:41 +0000

    Change board to a list of 3 lists.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-board = ["_", "|", "_", "|", "_"]
+board = [
+    ["_", "|", "_", "|", "_"],
+    ["_", "|", "_", "|", "_"],
+    [" ", "|", " ", "|"," "]
+    ]
+
+def print_board():
+    for row in board:
+        print(*row)
 
 loop_counter = 1
-
-while loop_counter < 3:
+user_choice = input("Choose X or O: ").upper()
+user_colum_decision = input("In which colum you want to put? Choose 0/2/4: ")
+user_row_decision = input("In which row you want to put? Choose 0/2/4: ")
+while loop_counter < 2:
     loop_counter += 1
-    print(*board)
-    if loop_counter == 3:
-        print(" ", "|", " ", "|"," ")
-
+    print_board()
