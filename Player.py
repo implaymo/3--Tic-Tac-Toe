4a0b902e9cb355bb94c0b41a79dcc19d7e7fe8c8
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-07 18:06:20 +0000

    Instead of creating a class for each player, i created a Player class. With this i will be able to create different players from this class.

--- Player.py
+++ Player.py
@@ -3,9 +3,6 @@ while Player1_choice != "X" and Player1_choice != "O":
     print("Wrong choice. You must choose 'X' or 'O'")
     Player1_choice = input("Choose X or O: ").upper()
     
-column = [0,2,4]
-row = [0,1,2]
-    
 Player1_column_decision = int(input("In which column do you want to put? Choose 0/2/4: "))
 while Player1_column_decision not in column:
     print("Wrong choice. You must choose 0, 2 or 4: ") 
@@ -16,3 +13,11 @@ while Player1_row_decision not in row:
     print("Wrong choice. You must choose 0, 1 or 2: ") 
     Player1_row_decision = int(input("In which row do you want to put? Choose 0/1/2: "))
 
+class Player():
+    def __init__(self) -> None:
+        self.column = [0,2,4]
+        self.row = [0,1,2]
+    
+    
+    def player_play(self):
+        pass
