3668c0c591cab544c975a7aa95556b5297b8678e
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-07 17:46:35 +0000

    Manage to print board with the choice of the user and handled cases when user gives a wrong answer.

--- main.py
+++ main.py
@@ -7,11 +7,28 @@ board = [
 def print_board():
     for row in board:
         print(*row)
+        
 
-loop_counter = 1
+
 user_choice = input("Choose X or O: ").upper()
-user_colum_decision = input("In which colum you want to put? Choose 0/2/4: ")
-user_row_decision = input("In which row you want to put? Choose 0/2/4: ")
-while loop_counter < 2:
-    loop_counter += 1
-    print_board()
+while user_choice != "X" and user_choice != "O":
+    print("Wrong choice. You must choose 'X' or 'O'")
+    user_choice = input("Choose X or O: ").upper()
+    
+column = [0,2,4]
+row = [0,1,2]
+    
+user_column_decision = int(input("In which column do you want to put? Choose 0/2/4: "))
+while user_column_decision not in column:
+    print("Wrong choice. You must choose 0, 2 or 4: ") 
+    user_column_decision = int(input("In which column do you want to put? Choose 0/2/4: "))
+    
+user_row_decision = int(input("In which row do you want to put? Choose 0/1/2: "))
+while user_row_decision not in row:
+    print("Wrong choice. You must choose 0, 1 or 2: ") 
+    user_row_decision = int(input("In which row do you want to put? Choose 0/1/2: "))
+        
+
+board[user_row_decision][user_column_decision] = user_choice
+
+print_board()
