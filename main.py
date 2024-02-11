aae329195e04fab8a696b63c4d52656ea7907a37
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-11 11:22:53 +0000

    Make mandatory player1 and player2 sign choice be different.

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ board = Board()
 player1 = Player(1)
 player2 = Player(2)
 
-player1.sign_choice()
-player2.sign_choice()
+player1_choice = player1.sign_choice()
+player2_choice = player2.sign_choice()
+if player2_choice == player1_choice:
+    print("Player 2, you must choose a different sign than Player 1.")
+    player2_choice = player2.sign_choice()
+
+
+
+
 while True:
     player1.play()
     board.add_signs(player1.player_row_decision, player1.player_column_decision, player1.player_choice)
