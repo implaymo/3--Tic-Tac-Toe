08a37b205b7f1fa118a65c09e9567c315c6aa90d
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-11 10:32:04 +0000

    Handle cases where user trys to choose a position already taken. Change made in add_signs()

--- board.py
+++ board.py
@@ -8,8 +8,11 @@ class Board():
 
         
     def add_signs(self, row, column, user_choice):
-       self.board[row][column] = user_choice
-       
+        if self.board[row][column] == "X" or self.board[row][column]  == "O":
+            print("Position already choosen. Try again.")
+        else: 
+            self.board[row][column]  = user_choice
+
     def print_board(self):
         for row in self.board:
             print(*row)
